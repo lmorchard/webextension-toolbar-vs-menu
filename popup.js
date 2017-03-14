d3609897d96db7785fcf6fc17d80806f3dd3bce1
Les Orchard <me@lmorchard.com>
2017-03-14 16:29:48 -0400

    Remove shadow in menu panel; add close button

--- popup.js
+++ popup.js
@@ -2,16 +2,18 @@ const NARROW_MIN_WIDTH = 320;
 
 log('init start ------------------------------');
 
-document.querySelectorAll('button.make-active').forEach(el =>
+const $$ = (sel, cb) => document.querySelectorAll(sel).forEach(cb);
+
+$$('button.close', el => el.addEventListener('click', ev => window.close()));
+
+$$('button.make-active', el =>
   el.addEventListener('click', ev => {
-    document.querySelectorAll('.panel.active').forEach(subEl => subEl.classList.remove('active'));
-    document.querySelector(`#${el.getAttribute('data-target')}.panel`).classList.add('active');
+    $$('.panel.active', subEl => subEl.classList.remove('active'));
+    $$(`#${el.getAttribute('data-target')}.panel`, subEl => subEl.classList.add('active'));
   })
 );
 
-window.addEventListener('load', ev => {
-  log('load');
-});
+window.addEventListener('load', ev => log('load'));
 
 // HACK: debounce resize event handling with a flag & requestAnimationFrame
 // https://developer.mozilla.org/en-US/docs/Web/Events/resize
